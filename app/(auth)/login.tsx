7e14698324ac9ad73c4ea3b8981edb0c7c309acf
Samuel-Learnity <samuel@learnity.app>
2024-03-17 20:27:49 +0100

    ui login exemple

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,14 +1,51 @@
-import { View, Text } from 'react-native';
-import React from 'react';
-import { Button } from 'tamagui';
-import { Container } from '~/tamagui.config';
+import React, { useState } from "react";
+import { H1, Input, XStack, YStack, Text } from "tamagui";
+import { useNavigation } from "expo-router";
 
-const login = () => {
+const Login = () => {
+  const navigation = useNavigation()
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const goToRegister = () => {
+    // Obligé d'enlever le typage avec ce commentaire sinon y a une erreur, c'est normal tqt
+
+    // @ts-ignore
+    navigation.navigate('register');
+  }
   return (
-    <Container>
-      <Text>Coucou</Text>
-    </Container>
+    <YStack flex={1}
+            justifyContent={"center"}
+            gap={"$4"}
+    >
+      <H1 textAlign="left" color={'black'}>Welcome back.</H1>
+
+      <XStack>
+        <Input flex={1} size={`$4`} marginHorizontal={"$4"}
+               placeholder={`Email`}
+               value={email}
+               onChangeText={e => setEmail(e)} />
+      </XStack>
+
+      <XStack>
+        <Input
+          flex={1} size={`$4`}
+          marginHorizontal={"$4"}
+          placeholder={`Password`}
+          secureTextEntry={true}
+          value={password}
+          onChangeText={e => setPassword(e)}
+        />
+      </XStack>
+
+      <XStack paddingHorizontal={"$4"} paddingVertical={"$4"}>
+        <Text>
+          Not an account yet ?{" "}
+          <Text textDecorationLine={"underline"} onPress={goToRegister}>Sign Up Here</Text>
+        </Text>
+      </XStack>
+    </YStack>
   );
 };
 
-export default login;
+export default Login;
